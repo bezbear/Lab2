72397a64d253c113545e7e37799f49f754840154
caccumcum <thienguyend@gmail.com>
2014-08-07 11:03:24 -0400

    Update show.php

--- view/show.php
+++ view/show.php
@@ -10,10 +10,48 @@ require '../model/user.php';
 require '../model/Tweet.php';
 
 function showArray($tweets) {
+    foreach ($tweets as $obj) {
+        echo $obj->created_at . '<br>';
+        echo $obj->user->screen_name . '<br>';
+        echo $obj->text . '<br>';
+        echo '<b>Friends</b>: ' . $obj->user->friends_count . ' I ';
+        echo '<b>Followers</b>: ' . $obj->user->followers_count . ' I ';
+        echo '<b>Retweeted</b>: ' . $obj->retweet_count . '<br>';
+        echo '<img src=' . $obj->user->profile_image_url . '/><br>';
+        echo '<b>Location</b>: ' . $obj->user->location . '<br>';
+        echo '<hr>';
+    }
+}
 
-    foreach ($tweets as $tweet) {
-        $user = new User($tweet->user->screen_name, $tweet->user->friends_count, $tweet->user->followers_count, $tweet->user->profile_image_url,$tweet->user->location);
-        $twt=new Tweet($tweet->created_at, $user, $tweet->text, $tweet->retweet_count);
+function showArray2($tweets) {
+    foreach ($tweets as $obj) {
+        $i++;
+        $user = new User($obj->user->screen_name,
+            $obj->user->friends_count,
+            $obj->user->followers_count,
+            $obj->user->profile_image_url,
+            $obj->user->location
+            );
+        $twt = new Tweet($obj->created_at, $user, $obj->text, $obj->retweet_count);
         $twt->display();
+    }
+}
+
+
+// Takes an array of Tweet objects and displays them. 
+function showArray3($allTweets) {
+    
+    $tweetsFound = sizeof($allTweets);
+    echo "Tweets found: $tweetsFound<br><br><br>";
+    if ($tweetsFound > 0) {
+        echo '<table>';
+        foreach ($allTweets as $theTweet) {
+            echo '<tr><td>';
+            $theTweet->display();
+            echo '</td></tr>';
+        }
+        echo '</table>';
+    } else {
+        echo "No tweets found<br>";
     }
 }
